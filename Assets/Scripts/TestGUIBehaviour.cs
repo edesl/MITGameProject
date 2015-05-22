dd69f659a65588ee5636119d36552e951be4bc62
Esteban Salazar <edsl.imk@gmail.com>
2015-05-22 16:45:36 -0500

    Añadidos escena y script de prueba para UI

---
+++ Assets/Scripts/TestGUIBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGUIBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ButtonClicked(){
+
+		Debug.Log ("CLICKED!");
+	}
+
+	public void TwoButtonClicked () {
+
+		Debug.Log ("2 CLICKED!");
+	}
+}
